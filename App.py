5328d7a1f357b0bb991ff1f5d69de00529bb8ca0
Leonardo <lmedeiros045@gmail.com>
2024-12-24 16:16:46 -0300

    binding a port

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+import os
 import fastapi
 from fastapi.responses import HTMLResponse
 
@@ -6,4 +7,10 @@ webApi = fastapi.FastAPI()
 @webApi.get("/")
 def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("API_PORT", 8000))
+    uvicorn.run("App:webApi", host="127.0.0.1", port=port, reload=True)
 
