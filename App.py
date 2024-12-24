13abc8e099d86b2a5e461441bf7ff1605b7ef271
Leonardo <lmedeiros045@gmail.com>
2024-12-24 18:05:16 -0300

    making few tests

--- App.py
+++ App.py
@@ -1,8 +1,11 @@
 import os
+
 import fastapi
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 webApi = fastapi.FastAPI()
+webApi.mount("/", StaticFiles(directory="./FrontEnd/pages/", html=True), name="web")
 
 @webApi.get("/")
 def root():
@@ -10,8 +13,10 @@ def root():
 
 
 if __name__ == "__main__":
+    # starts the API server (to register the endpoints)
     import uvicorn
     port = int(os.environ.get("API_PORT", 8000))
     host = os.environ.get("APP_HOST", "127.0.0.1")
     uvicorn.run("App:webApi", host=host, port=port, reload=True)
-
+    
+    
