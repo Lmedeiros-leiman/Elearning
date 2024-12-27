ab972ff6940864ff7b7753728ca198dbd11982f0
Leonardo <lmedeiros045@gmail.com>
2024-12-27 20:19:34 -0300

    cors allow all (temporary?)

--- setup.py
+++ setup.py
@@ -2,15 +2,27 @@ import os
 import fastapi
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 WebServer = fastapi.FastAPI()
-
+WebServer.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #
 # API routes
 #
 @WebServer.get("/api/")
 async def ApiRoute():
+
+    return {"message": "Hello World"}
     pass
 
 
@@ -39,6 +51,6 @@ if __name__ == "__main__":
 
     port = int(os.environ.get("API_PORT", 8000))
     host = os.environ.get("APP_HOST", "127.0.0.1")
-    debug = os.environ.get("DEBUG", False)
+    debug = os.environ.get("PRODUCTION", True)
 
     uvicorn.run("setup:WebServer", host=host, port=port, reload=debug) 
